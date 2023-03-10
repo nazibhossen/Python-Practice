25cdc2850cfd53adeeb8add4beb8df6c6eea9c1e
nazibhossen <nazibhossen1@gmail.com>
2023-03-10 19:49:34 +0600

    new code

---
+++ random_1.py
@@ -0,0 +1,7 @@
+#Example 1:
+import random
+print(random.randint(3, 9))
+
+#Example 2:
+from random import randint
+print(randint(1,7))
---
+++ test3.py
@@ -0,0 +1,18 @@
+# def compare(x,y):
+#     if x > y:
+#         return  x
+#     else:
+#         return y
+
+# a = input()
+# b = input()
+# print(compare(a,b))
+
+
+def sum(x):
+    res = 0
+    for i in range(x):
+        res += i
+    return res
+
+print(sum(6))
